f9bd0ea42566de9d481e8e221b42a343cb681b0a
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-08 12:56:28 +0530

    Added : proper indentation and comments

--- src/com/bridgelabz/algorithmPrograms/BubbleSort.java
+++ src/com/bridgelabz/algorithmPrograms/BubbleSort.java
@@ -3,28 +3,34 @@ package com.bridgelabz.algorithmPrograms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BubbleSort {
+public class BubbleSort 
+{
 
 	//To sort using bubble sort algorithm 
-	public static void bubbleSort(List<Integer> list) {
+	public static void bubbleSort(List<Integer> list) 
+	{
 
-	    int n = list.size();
+	    int n = list.size(); //size of the list
 	    int temp = 0;
 
-	    for(int i = 0; i < n-1; i++) {
-	        for (int j = 0; j < (n - i - 1); j++) {
+	    for(int index1 = 0; index1 < n-1; index1++) 
+	    {
+	        for (int index2 = 0; index2 < (n - index1 - 1); index2++) 
+	        {
 
-	            if (list.get(j) > list.get(j+1)) {
-	                temp = list.get(j);
-	                list.set(j,list.get(j+1));
-	                list.set(j+1, temp);
+	            if (list.get(index2) > list.get(index2+1)) 
+	            {
+	            	// swapping the values 
+	                temp = list.get(index2);
+	                list.set(index2,list.get(index2+1));
+	                list.set(index2+1, temp);
 	            }
 
 	        }
 	    }
 	}
 	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
 	    list.add(1);
 	    list.add(6);
 	    list.add(3);
